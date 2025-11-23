858c3bfc9c3c9dbc009b60c3b91f74b306cd69ef
Rob Pungello <rob@rpun.io>
2025-11-23 12:39:48 -0500

    Use sqlite:memory db for testing

--- config/database.php
+++ config/database.php
@@ -31,10 +31,9 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
+        'testing' => [
             'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => ':memory:',
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
