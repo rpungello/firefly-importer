3496622d30d210f0ac3ca57bcb6bd1a8b5f3f36a
Rob Pungello <rob@rpun.io>
2025-11-24 11:44:14 -0500

    Handle negative amounts

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\Filetype;
+use Decimal\Decimal;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -41,11 +42,24 @@ class Account extends Model
 
     public function mapTransaction(array $transaction): array
     {
-        $result = [
-            'source_id' => $this->firefly_id,
-        ];
+        $result = [];
+        $amount = new Decimal(0);
+
         foreach ($this->fields as $field) {
             $result[$field->destination_field] = $field->mapFromTransaction($transaction);
+
+            if ($field->destination_field === 'amount') {
+                $amount = new Decimal($result['amount']);
+            }
+        }
+
+        if ($amount->isNegative()) {
+            $result['amount'] = $amount->negate()->toFixed(2);
+            $result['destination_id'] = $this->firefly_id;
+            $result['source_name'] = $result['destination_name'];
+            unset($result['destination_name']);
+        } else {
+            $result['source_id'] = $this->firefly_id;
         }
 
         return $result;
